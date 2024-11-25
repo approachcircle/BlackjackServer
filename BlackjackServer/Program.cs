3cf2f62d7228682cf1093878c5664b21eb88b34e
approachcircle <osc-mcd@protonmail.com>
2024-11-25 01:36:07 +0000

    add SignalR to services

--- BlackjackServer/Program.cs
+++ BlackjackServer/Program.cs
@@ -5,6 +5,7 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddSignalR();
         var app = builder.Build();
 
         app.MapGet("/", () => "Hello World!");
