286a815b9be38292afb5576a33dad96eb1ec60d3
approachcircle <osc-mcd@protonmail.com>
2024-11-27 03:35:16 +0000

    increase logging level to debug

--- BlackjackServer/Program.cs
+++ BlackjackServer/Program.cs
@@ -6,6 +6,8 @@ public static class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddSignalR();
+        builder.Logging.AddFilter("Microsoft.AspNetCore.SignalR", LogLevel.Debug);
+        builder.Logging.AddFilter("Microsoft.AspNetCore.Http.Connections", LogLevel.Debug);
         var app = builder.Build();
         app.MapHub<OnlinePlayHub>("/online");
         app.Run();
